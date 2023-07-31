e075046e6ccc49f1a338a62170f10adce2bc0a15
maxpurrp <121128811+maxpurrp@users.noreply.github.com>
2023-07-31 03:56:34 +0300

    'add_rules_and_info'

--- SeaBattle.py
+++ SeaBattle.py
@@ -1,4 +1,20 @@
 import curses
+import curses.panel
+
+RULES = ['You have 10 ships. 4 single-deck, 3 double-deck, 2 three-deck and 1 four-deck',
+         'Ships during deployment should not touch each other, there should be at least one cell between them',
+         'Field size - 10 by 10',
+         'Your task is to correctly place the ships on the field and win your opponent']
+ABOUT_ME = ['My name is Max Siomin',
+            '25 years',
+            'Python Backend Developer',
+            'Diligent and hardworking',
+            'I love the x y coordinate system']
+
+class Game:
+    def __init__(self) -> None:
+        pass
+
 
 
 class Menu:
@@ -7,7 +23,6 @@ class Menu:
 
     def _start(self):
         menu = ['PLAY GAME', 'RULES', 'ABOUT', 'EXIT']
-
         curses.curs_set(0)
         curses.start_color()
         curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
@@ -19,17 +34,23 @@ class Menu:
             key = self.stdscr.getch()
             if key in (curses.KEY_UP, ord('w')) and current_row > 0:
                 current_row -= 1
-            if key in (curses.KEY_DOWN, ord('s')) and current_row < len(menu)-1:
+            if key in (curses.KEY_DOWN, ord('s')) and current_row < len(menu) - 1:
                 current_row += 1
             if key == curses.KEY_ENTER or key in [10, 13]:
-                self._print_center("You won")
-                self.stdscr.getch()
+                if current_row == 0:
+                    self._print_center("You won 1")
+                elif current_row == 1:
+                    self._print_rules()
+                elif current_row == 2:
+                    self._print_about()
                 if current_row == len(menu) - 1:
                     break
+                self.stdscr.getch()
             self._print_menu(current_row, menu)
 
     def _print_menu(self, curr_ind, menu):
         self.stdscr.clear()
+        self.stdscr.border()
         h, w = self.stdscr.getmaxyx()
         for idx, elem in enumerate(menu):
             x = w//2 - len(elem)//2
@@ -41,8 +62,35 @@ class Menu:
             else:
                 self.stdscr.addstr(y, x, elem)
 
+    def _print_rules(self):
+        self.stdscr.clear()
+        self.stdscr.border()
+        h, w = self.stdscr.getmaxyx()
+        for elem in range(len(RULES)):
+            x = w//2 - len(RULES[elem])//2
+            y = h//2 - len(RULES) + elem
+            self.stdscr.addstr(y, x, RULES[elem])
+            h, w = self.stdscr.getmaxyx()
+            x = w//2
+            y = h//2
+        self.stdscr.addstr(y + 3, x - 15, 'Press enter to return in menu')
+
+    def _print_about(self):
+        self.stdscr.clear()
+        self.stdscr.border()
+        h, w = self.stdscr.getmaxyx()
+        for elem in range(len(ABOUT_ME)):
+            x = w//2 - len(ABOUT_ME[elem])//2
+            y = h//2 - len(ABOUT_ME) + elem
+            self.stdscr.addstr(y, x, ABOUT_ME[elem])
+            h, w = self.stdscr.getmaxyx()
+            x = w//2
+            y = h//2
+        self.stdscr.addstr(y + 3, x - 15, 'Press enter to return in menu')
+
     def _print_center(self, text):
         self.stdscr.clear()
+        self.stdscr.border()
         h, w = self.stdscr.getmaxyx()
         x = w//2 - len(text)//2
         y = h//2
